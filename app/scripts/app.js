489cea92bf2204deaad4138f9164169a76869eec
Hsin <shinsonh@gmail.com>
2014-08-22 10:18:06 -0400

    deployed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ angular.module('d3', [])
       var scriptTag = $document[0].createElement('script');
       scriptTag.type = 'text/javascript'; 
       scriptTag.async = true;
-      scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+      scriptTag.src = 'bower_components/d3.v3.min.js';
       scriptTag.onreadystatechange = function () {
         if (this.readyState === 'complete') onScriptLoad();
       }
